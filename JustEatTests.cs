bffee79f4693f935ab187cb42b3eb3b0bb1df4e1
James Flynn <jamesdeclonflynn@hotmail.com>
2025-09-29 14:57:00 +0100

    Changes to reduce number of lines and simplify process of changing what is being searched.

--- JustEatTests.cs
+++ JustEatTests.cs
@@ -4,11 +4,14 @@ namespace JustEatTestsProject
 {
     public class JustEatTests
     {
-        [Test]
-        public async Task NavigateToJustEatHomePage()
+        private readonly string address = "AR511AA";
+        private readonly string expectedAddress = "Testville, AR51 1AA";
+        private readonly string category = "Alcohol";
+
+        private async Task <(IPage, IBrowser)> Setup()
         {
             // ARRANGE
-            using var playwright = await Playwright.CreateAsync();
+            var playwright = await Playwright.CreateAsync();
             var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
             {
                 Headless = false
@@ -17,6 +20,15 @@ namespace JustEatTestsProject
             // ACT
             var page = await browser.NewPageAsync();
             await page.GotoAsync("https://www.just-eat.co.uk/");
+            await page.Locator("[data-test-id='actions-necessary-only']").ClickAsync(); // Accept cookies
+
+            return (page, browser);
+        }
+
+        [Test]
+        public async Task NavigateToJustEatHomePage()
+        {
+            var (page, browser) = await Setup();
 
             // ASSERT
             Assert.That(await page.TitleAsync(), Is.EqualTo("Order takeaway online from 30,000+ food delivery restaurants | Just Eat"));
@@ -24,24 +36,16 @@ namespace JustEatTestsProject
         }
 
         [Test]
-        public async Task ShowSearchResultsTestville()
+        public async Task ShowAddressSearchResults()
         {
-            //ARRANGE 
-            using var playwright = await Playwright.CreateAsync();
-            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                Headless = false
-            });
+            // ARRANGE
+            var (page, browser) = await Setup();
 
             // ACT
-            var page = await browser.NewPageAsync();
-            await page.GotoAsync("https://www.just-eat.co.uk/");
-            await Task.Delay(2000); // Wait for 2 seconds to ensure the page is fully loaded
-            await page.Locator("[data-test-id='actions-necessary-only']").ClickAsync(); // Accept cookies
             await page.Locator("text='Search'").ClickAsync();
-            await page.Locator(".VO0jE").FillAsync("AR511AA");
-            await page.Locator("text='Testville, AR51 1AA'").ClickAsync();
-            await page.Locator("text='Order from 3869 places'").WaitForAsync();
+            await page.Locator(".VO0jE").FillAsync(address);
+            await page.Locator($"text='{expectedAddress}'").ClickAsync();
+            await page.Locator("text='Order from 3871 places'").WaitForAsync();
 
             // ASSERT
             Assert.That(await page.TitleAsync(), Is.EqualTo("Restaurants and takeaways in , Testville | Just Eat"));
@@ -51,22 +55,13 @@ namespace JustEatTestsProject
         [Test]
         public async Task TryOrderPizza()
         {
-            //ARRANGE 
-            using var playwright = await Playwright.CreateAsync();
-            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                Headless = false
-            });
+            // ARRANGE
+            var (page, browser) = await Setup();
 
             // ACT
-            var page = await browser.NewPageAsync();
-            await page.GotoAsync("https://www.just-eat.co.uk/");
-            await page.Locator("[data-test-id='actions-necessary-only']").ClickAsync(); // Accept cookies
-            await page.Locator("text='Search'").ClickAsync();
-            await page.Locator(".VO0jE").FillAsync("AR511AA");
-            await page.Locator("text='Testville, AR51 1AA'").ClickAsync();
-            await page.Locator("text='Order from 3869 places'").WaitForAsync();
-            await page.Locator("text='CrossSelling Pizzas Test - Do Not Edit'").ClickAsync();
+            await page.GetByPlaceholder("Full address").FillAsync(address);
+            await page.Locator($"text='{expectedAddress}'").ClickAsync();
+            await page.GetByText("CrossSelling Pizzas Test - Do Not Edit").ClickAsync();
             await page.Locator("._3wa4B:has-text('Pizzas')").ClickAsync();
             await page.Locator("text='Margherita'").ClickAsync();
             await page.Locator(".single-selection-style_text__EFHuZ:has-text('13\"')").ClickAsync();
@@ -77,6 +72,22 @@ namespace JustEatTestsProject
 
             // ASSERT
             Assert.That(await page.TitleAsync(), Is.EqualTo("Just Eat | Sign in/Login")); // screen after checkout clicked
+            await browser.CloseAsync();
+        }
+
+        [Test]
+        public async Task FilterCuisine()
+        {
+            //ARRANGE 
+            var (page, browser) = await Setup();
+
+            // ACT
+            await page.GetByPlaceholder("Full address").FillAsync(address);
+            await page.Locator($"text='{expectedAddress}'").ClickAsync();
+            await page.Locator($"text='{category}'").ClickAsync();
+
+            // ASSERT
+            Assert.That(await page.Locator("[data-qa='list-all-open-title']").InnerTextAsync(), Does.Contain($"{category}"));
             await browser.CloseAsync();
         }
     }
